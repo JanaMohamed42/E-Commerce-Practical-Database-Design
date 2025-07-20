b2fa5bfa77802d083086de1c351bcb8b0008ab7d
JanaMohamed42 <gamo81838838@gmail.com>
2025-07-20 17:48:09 +0300

    Update lockquantity.sql

--- lockquantity.sql
+++ lockquantity.sql
@@ -1,18 +1,18 @@
 START TRANSACTION;
 
-SELECT stock_product
+SELECT stock_quantity
 FROM Product
 WHERE product_id = 211
 FOR UPDATE;
 
 SET @requested_qty = 4;
 SET @current_stock = (
-  SELECT stock_product FROM Product WHERE product_id = 211
+  SELECT stock_quantity FROM Product WHERE product_id = 211
 );
 
 IF @current_stock >= @requested_qty THEN
   UPDATE Product
-  SET stock_product = stock_product - @requested_qty
+  SET stock_quantity = stock_quantity - @requested_qty
   WHERE product_id = 211;
 
   COMMIT;
